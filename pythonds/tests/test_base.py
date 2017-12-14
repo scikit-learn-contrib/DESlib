7bbdd7e16993737b6d6dbc66f7616e2db6fb8028
Luiz Gustavo Hafemann <luiz.gh@gmail.com>
2017-12-13 21:39:55 -0500

    Adding some use cases for the base DS class

--- pythonds/tests/test_base.py
+++ pythonds/tests/test_base.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 from pythonds.base import DS
 import numpy as np
+import pytest
 
 
 def test_all_classifiers_agree():
@@ -20,6 +21,32 @@ def test_not_all_classifiers_agree():
 
     x = np.ones((1, 10))
     assert not ds._all_classifier_agree(x)
+
+
+def create_some_classifiers():
+    return [create_base_classifier(return_value=1)] * 10
+
+
+@pytest.mark.parametrize('k', [0, 1, -1])
+def test_check_k(k):
+    pool_classifiers = create_some_classifiers()
+
+    with pytest.raises(ValueError):
+        DS(pool_classifiers, k=k)
+
+
+@pytest.mark.parametrize('k', ['a', 2.5])
+def test_check_k(k):
+    pool_classifiers = create_some_classifiers()
+
+    with pytest.raises(TypeError):
+        DS(pool_classifiers, k=k)
+
+
+@pytest.mark.parametrize('k, ih_k', [(2, 3), (5, 7)])
+def test_valid_IH_k(k, ih_k):
+    with pytest.raises(ValueError):
+        DS([create_base_classifier(1)], k=k, safe_k=ih_k)
 
 
 def create_base_classifier(return_value, return_prob=None):
