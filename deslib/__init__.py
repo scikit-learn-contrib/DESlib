103414725d4e87f29a4e652b92e025a944858a6f
Rafael <rafaelmenelau@gmail.com>
2018-01-18 00:54:24 -0500

    testing new configuration for RTD and travis

---
+++ deslib/__init__.py
@@ -0,0 +1,23 @@
+"""A Python library for Dynamic Ensemble Selection.
+
+``DESlib`` is a library containing the implementation of the state-of-the art
+dynamic classifier and ensemble selection techniques. The library also provides
+some static ensemble methods that are used as baseline comparison.
+
+Subpackages
+-----------
+des
+    The implementation of several DES techniques.
+
+dcs
+    The implementation of several DCS techniques.
+
+static
+    The implementation of baseline ensemble methods.
+
+util
+    A collection of aggregation functions and diversity measures for ensemble of classifiers.
+"""
+
+# list of all modules available in the library
+__all__ = ['des', 'dcs', 'static', 'util', 'tests']
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
+
 
 setup(name='DESlib',
       version='0.1',
-      description='Dynamic Classifier Selection methods',
+      url='https://github.com/Menelau/DESlib',
+      maintainer='Rafael M. O. Cruz, L. G. Hafemann',
+      maintainer_email='rafaelmenelau@gmail.com',
+      description='Implementation of Dynamic Ensemble Selection methods',
       author='Rafael M. O. Cruz',
       author_email='rafaelmenelau@gmail.com',
-      packages=['deslib'],
-     )
+      packages=find_packages())
+
