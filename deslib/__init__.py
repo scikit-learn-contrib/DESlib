9d0ac4e7db224bbe66295e4cc07bbb6024e2ac60
Rafael <rafaelmenelau@gmail.com>
2018-01-18 01:24:45 -0500

    Removing version file from __init__.py

--- deslib/__init__.py
+++ deslib/__init__.py
@@ -1,25 +1,23 @@
 """A Python library for Dynamic Ensemble Selection.
 
-``DESlib`` is a library containig the implementation of the state-of-the art 
+``DESlib`` is a library containing the implementation of the state-of-the art
 dynamic classifier and ensemble selection techniques. The library also provides
 some static ensemble methods that are used as baseline comparison.
 
 Subpackages
 -----------
 des
-    The mplementation of several DES techniques.
+    The implementation of several DES techniques.
 
 dcs
-    The mplementation of several DCS techniques.
+    The implementation of several DCS techniques.
 
 static
-    The Implementation of baseline ensemble methods.
+    The implementation of baseline ensemble methods.
 
 util
     A collection of aggregation functions and diversity measures for ensemble of classifiers.
 """
-
-from ._version import __version__
 
 # list of all modules available in the library
 __all__ = ['des', 'dcs', 'static', 'util']
