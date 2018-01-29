6001b10236e6033143f57988c5b588b31e8392a5
Luiz Gustavo Hafemann <luiz.gh@gmail.com>
2018-01-29 15:20:37 -0500

    First release

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@
 
 from distutils.core import setup
 from setuptools import find_packages
+import os
+import io
 
+setup_path = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(setup_path, 'README.rst'), 'r') as f:
+    README = f.read()
 
 setup(name='DESlib',
       version='0.1',
@@ -10,7 +15,27 @@ setup(name='DESlib',
       maintainer='Rafael M. O. Cruz, L. G. Hafemann',
       maintainer_email='rafaelmenelau@gmail.com',
       description='Implementation of Dynamic Ensemble Selection methods',
+      long_description=README,
       author='Rafael M. O. Cruz',
       author_email='rafaelmenelau@gmail.com',
+      license='BSD 3-clause "New" or "Revised License"',
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      install_requires=[
+          'scikit-learn',
+          'numpy',
+          'scipy',
+      ],
+      python_requires='>=3',      
+
       packages=find_packages())
 
