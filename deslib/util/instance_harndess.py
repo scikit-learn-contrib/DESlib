4d8b1d794fe315c1f8532c88720c75201a8a0017
Rafael <rafaelmenelau@gmail.com>
2018-03-27 00:45:33 -0400

    Moving the function to measure instance hardness from the DS base class to the util module.

---
+++ deslib/util/instance_harndess.py
@@ -0,0 +1,49 @@
+from scipy.stats import mode
+import numpy as np
+
+"""
+This file contains the implementation of different functions to measure instance hardness. Instance hardness can be
+defined as the likelihood that a given sample will be misclassified by different learning algorithms. 
+
+References
+----------
+Smith, M.R., Martinez, T. and Giraud-Carrier, C., 2014. An instance level analysis of data complexity.
+Machine learning, 95(2), pp.225-256
+"""
+
+def hardness_region_competence(neighbors_idx, labels, safe_k):
+    """Calculate the Instance hardness of the sample based on its neighborhood. The sample is deemed hard to
+    classify when there is overlap between different classes in the region of competence. This method does not
+    takes into account the target label of the test sample
+
+    This hardness measure is used to select
+    whether use DS or use the KNN for the classification of a given query sample
+
+    Parameters
+    ----------
+    neighbors_idx : array of shape = [n_samples_test, k]
+                    Indices of the nearest neighbors for each considered sample
+
+    labels : array of shape = [n_samples_train]
+            labels associated with each training sample
+
+    safe_k : int
+             Number of neighbors used to estimate the hardness of the corresponding region
+
+    Returns
+    -------
+    hardness : The calculated IH level of the corresponding sample
+
+    Reference
+    ----------
+    Smith, M.R., Martinez, T. and Giraud-Carrier, C., 2014. An instance level analysis of data complexity.
+    Machine learning, 95(2), pp.225-256
+    """
+    if neighbors_idx.ndim < 2:
+        neighbors_idx = np.atleast_2d(neighbors_idx)
+
+    neighbors_y = labels[neighbors_idx[:, :safe_k]]
+    _, num_majority_class = mode(neighbors_y, axis=1)
+    hardness = ((safe_k - num_majority_class) / safe_k).reshape(-1, )
+
+    return hardness
