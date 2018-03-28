29a4b67316423f628a4de62e75291c034246e529
Rafael <rafaelmenelau@gmail.com>
2018-03-28 02:56:35 -0400

    Updated test routines considering for predictions in batch

--- deslib/tests/dcs/test_base.py
+++ deslib/tests/dcs/test_base.py
@@ -33,19 +33,19 @@ def test_valid_diff_threshold_value(diff_thresh):
 def test_select_best(competences, expected):
     pool_classifiers = create_pool_classifiers()
     dcs_test = DCS(pool_classifiers, selection_method='best')
-    selected_clf = dcs_test.select(competences)
-    assert selected_clf == expected
+    selected_clf = dcs_test.select(np.array(competences))
+    assert np.allclose(selected_clf, expected)
 
 
-@pytest.mark.parametrize('competences, expected', [([1.0, 1.0, 0.5], [0, 1]),
-                                                   ([0.8, 0.9, 0.9], [1, 2]),
-                                                   ([0.15, 0.15, 0.15], [0, 1, 2]),
-                                                   ([0.0, 0.0, 0.0], [0, 1, 2])])
+@pytest.mark.parametrize('competences, expected', [([1.0, 1.0, 0.5], [True, True, False]),
+                                                   ([0.8, 0.9, 0.9], [False, True, True]),
+                                                   ([0.15, 0.15, 0.15], [True, True, True]),
+                                                   ([0.0, 0.0, 0.0], [True, True, True])])
 def test_select_all(competences, expected):
     pool_classifiers = create_pool_classifiers()
     dcs_test = DCS(pool_classifiers, selection_method='all')
-    selected_clf = dcs_test.select(competences)
-    assert selected_clf == expected
+    selected_clf = dcs_test.select(np.array(competences))
+    assert np.allclose(selected_clf, expected)
 
 
 @pytest.mark.parametrize('competences, expected', [([1.0, 0.5, 0.5], 0), ([0.8, 0.9, 1.0], 2), ([0.0, 0.0, 0.15], 2)])
@@ -53,8 +53,8 @@ def test_select_diff(competences, expected):
     rng = np.random.RandomState(123456)
     pool_classifiers = create_pool_classifiers()
     dcs_test = DCS(pool_classifiers, selection_method='diff', diff_thresh=0.15, rng=rng)
-    selected_clf = dcs_test.select(competences)
-    assert selected_clf == expected
+    selected_clf = dcs_test.select(np.array(competences))
+    assert np.allclose(selected_clf, expected)
 
 
 @pytest.mark.parametrize('competences, expected', [([0.5, 0.5, 0.5], 1), ([0.8, 0.9, 1.0], 2), ([0.0, 0.10, 0.0], 1)])
@@ -62,8 +62,46 @@ def test_select_random(competences, expected):
     rng = np.random.RandomState(123456)
     pool_classifiers = create_pool_classifiers()
     dcs_test = DCS(pool_classifiers, selection_method='random', rng=rng)
+    selected_clf = dcs_test.select(np.array(competences))
+    assert np.allclose(selected_clf, expected)
+
+
+def test_select_best_batch():
+    competences = np.array([[1.0, 0.5, 0.5], [0.8, 0.9, 1.0], [0.0, 0.0, 0.15]])
+    expected = [0, 2, 2]
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers, selection_method='best')
     selected_clf = dcs_test.select(competences)
-    assert selected_clf == expected
+    assert np.array_equal(selected_clf, expected)
+
+
+def test_select_all_batch():
+    competences = np.array([[1.0, 1.0, 0.5], [0.8, 0.9, 0.9], [0.15, 0.15, 0.15], [0.0, 0.0, 0.0]])
+    expected = np.array([[True, True, False], [False, True, True], [True, True, True], [True, True, True]])
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers, selection_method='all')
+    selected_clf = dcs_test.select(competences)
+    assert np.array_equal(selected_clf, expected)
+
+
+def test_select_diff_batch():
+    competences = np.array([[1.0, 0.5, 0.5], [0.8, 0.9, 1.0], [0.0, 0.0, 0.15]])
+    expected = np.array([0, 2, 2])
+    rng = np.random.RandomState(123456)
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers, selection_method='diff', diff_thresh=0.15, rng=rng)
+    selected_clf = dcs_test.select(competences)
+    assert np.array_equal(selected_clf, expected)
+
+
+def test_select_random_batch():
+    competences = np.array([[0.5, 0.5, 0.5], [0.8, 0.9, 1.0], [0.0, 0.10, 0.0]])
+    expected = np.array([1, 2, 1])
+    rng = np.random.RandomState(123456)
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers, selection_method='random', rng=rng)
+    selected_clf = dcs_test.select(competences)
+    assert np.array_equal(selected_clf, expected)
 
 # ------------------------ Testing classify_instance and predict_proba -----------------
 
@@ -84,13 +122,32 @@ def test_classify_instance():
     assert predicted_label == expected
 
 
+def test_classify_instance_batch():
+    n_samples = 3
+    query = np.ones((n_samples, 2))
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers)
+    competences = np.random.rand(n_samples, dcs_test.n_classifiers)
+
+    dcs_test.estimate_competence = MagicMock(return_value=competences)
+    expected = []
+    for ind in range(n_samples):
+        expected.append(pool_classifiers[np.argmax(competences[ind, :])].predict(query)[0])
+
+    predictions = []
+    for clf in dcs_test.pool_classifiers:
+        predictions.append(clf.predict(query)[0])
+    predicted_label = dcs_test.classify_instance(query, np.tile(predictions, (3, 1)))
+    assert np.array_equal(predicted_label, expected)
+
+
 @pytest.mark.parametrize('competences, expected', [([0.6, 0.2, 0.6], 0),
                                                    ([0.5, 0.8, 0.5], 1)])
 def test_classify_instance_all(competences, expected):
     query = np.array([-1, 1])
     pool_classifiers = create_pool_classifiers()
     dcs_test = DCS(pool_classifiers, selection_method='all')
-    dcs_test.estimate_competence = MagicMock(return_value=competences)
+    dcs_test.estimate_competence = MagicMock(return_value=np.array(competences))
 
     predictions = []
     for clf in dcs_test.pool_classifiers:
@@ -98,6 +155,21 @@ def test_classify_instance_all(competences, expected):
     predicted_label = dcs_test.classify_instance(query, np.array(predictions))
     assert predicted_label == expected
 
+
+def test_classify_instance_all_batch():
+    competences = np.array([[0.6, 0.2, 0.6], [0.5, 0.8, 0.5]])
+    expected = [0, 1]
+    n_samples = 2
+    query = np.ones((n_samples, 2))
+    pool_classifiers = create_pool_classifiers()
+    dcs_test = DCS(pool_classifiers, selection_method='all')
+    dcs_test.estimate_competence = MagicMock(return_value=np.array(competences))
+
+    predictions = []
+    for clf in dcs_test.pool_classifiers:
+        predictions.append(clf.predict(query)[0])
+    predicted_label = dcs_test.classify_instance(query, np.tile(predictions, (n_samples, 1)))
+    assert np.array_equal(predicted_label, expected)
 
 def test_predict_proba_instance():
     query = np.array([-1, 1])
