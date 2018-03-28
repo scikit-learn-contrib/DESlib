dedcaa98373e76f518e203920e27ad491eca2b1d
Rafael <rafaelmenelau@gmail.com>
2018-03-28 03:30:23 -0400

    New file containing the instance_hardness test routines that were removed from the DS base class.

---
+++ deslib/tests/util/test_instance_hardness.py
@@ -0,0 +1,33 @@
+import pytest
+import numpy as np
+from deslib.util.instance_harndess import hardness_region_competence
+from deslib.tests.examples_test import y_dsel_ex1, neighbors_ex1
+
+
+# -----------------------Test routines for the Instance Hardness calculation--------------------
+@pytest.mark.parametrize('index, expected', [(0, 0.42),
+                                             (1, 0.28),
+                                             (2, 0.28)])
+def test_instance_hardness_region(index, expected):
+    y = y_dsel_ex1
+    k = 7
+    neighbors = neighbors_ex1[index, :]
+    IH = hardness_region_competence(neighbors, y, k)
+    assert np.isclose(IH, expected, atol=0.01)
+
+
+def test_instance_hardness_region_batch():
+    expected = np.array([0.42, 0.28, 0.28])
+    y = y_dsel_ex1
+    k = 7
+    neighbors = neighbors_ex1
+    IH = hardness_region_competence(neighbors, y, k)
+    assert np.allclose(IH, expected, atol=0.01)
+
+
+def test_instance_hardness_region_all_same():
+    y = y_dsel_ex1
+    k = 7
+    neighbors = np.array([0, 1, 2, 6, 7, 8, 13])
+    IH = hardness_region_competence(neighbors, y, k)
+    assert IH == 0.0
