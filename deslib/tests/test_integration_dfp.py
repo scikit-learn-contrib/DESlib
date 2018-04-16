b586320cec71510e8a8f5dafb4b99aea7e99280f
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2018-04-16 01:20:59 -0400

    Removing repeated test for the KNOP class

--- deslib/tests/test_integration_dfp.py
+++ deslib/tests/test_integration_dfp.py
@@ -144,9 +144,10 @@ def test_meta():
 def test_knop():
     pool_classifiers, X_dsel, y_dsel, X_test, y_test = setup_classifiers()
 
-    knop = KNOP(pool_classifiers, DFP=True)
-    knop.fit(X_dsel, y_dsel)
-    assert np.isclose(knop.score(X_test, y_test), 0.8606060606060606)
+    kne = KNOP(pool_classifiers, DFP=True)
+    kne.fit(X_dsel, y_dsel)
+
+    assert np.isclose(kne.score(X_test, y_test), 0.9030303030303031)
 
 
 def test_desknn():
@@ -238,13 +239,3 @@ def test_desknn_proba():
     probas = desknn.predict_proba(X_test)
     expected = np.load('deslib/tests/expected_values/desknn_probas_DFP.npy')
     assert np.allclose(probas, expected)
-
-
-def test_knop():
-    pool_classifiers, X_dsel, y_dsel, X_test, y_test = setup_classifiers()
-
-    kne = KNOP(pool_classifiers, DFP=True)
-    kne.fit(X_dsel, y_dsel)
-
-    assert np.isclose(kne.score(X_test, y_test), 0.9030303030303031)
-
