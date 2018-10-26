83ca14cac4a796841e6956fba9e9e9790ee2c94b
Luiz Gustavo Hafemann <luiz.gh@gmail.com>
2018-10-26 14:28:41 -0400

    Stacked model using softlabels

--- deslib/static/stacked.py
+++ deslib/static/stacked.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.base import ClassifierMixin, BaseEstimator
+from sklearn.ensemble import BaseEnsemble
+from sklearn.preprocessing import LabelEncoder
 
 
 class StackedClassifier(BaseEstimator, ClassifierMixin):
@@ -8,19 +10,42 @@ class StackedClassifier(BaseEstimator, ClassifierMixin):
         self.n_classifiers = len(self.pool_classifiers)
         self.meta_classifier = meta_classifier
 
+        if isinstance(self.pool_classifiers, BaseEnsemble):
+            self.base_already_encoded = True
+        else:
+            self.base_already_encoded = False
+
     def fit(self, X, y):
-        base_preds = self._predict_base(X)
-        self.meta_classifier.fit(base_preds, y)
+        # Check if base classifiers are not using LabelEncoder (the case for
+        # scikit-learn's ensembles):
+
+        y_ind = self._setup_label_encoder(y)
+        base_preds = self._predict_proba_base(X)
+        self.meta_classifier.fit(base_preds, y_ind)
 
     def predict(self, X):
-        base_preds = self._predict_base(X)
+        base_preds = self._predict_proba_base(X)
         return self.meta_classifier.predict(base_preds)
 
-    def _predict_base(self, X):
-        predictions = np.zeros((X.shape[0], self.n_classifiers),
-                               dtype=np.intp)
+    def _predict_proba_base(self, X):
+        probabilities = np.zeros(
+            (X.shape[0], self.n_classifiers, self.n_classes_))
 
         for index, clf in enumerate(self.pool_classifiers):
-            labels = clf.predict(X)
-            predictions[:, index] = labels
-        return predictions
+            probabilities[:, index] = clf.predict_proba(X)
+        return probabilities.reshape(X.shape[0],
+                                     self.n_classifiers * self.n_classes_)
+
+    def _setup_label_encoder(self, y):
+        self.enc_ = LabelEncoder()
+        y_ind = self.enc_.fit_transform(y)
+        self.classes_ = self.enc_.classes_
+        self.n_classes_ = self.classes_.size
+
+        return y_ind
+
+    def _encode_base_labels(self, y):
+        if self.base_already_encoded:
+            return y
+        else:
+            return self.enc_.transform(y)
