Fixing data leak (split x_train and x_dsel from x_train, not x)

examples/plot_random_forest.py
@@ -49,7 +49,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
 RF = RandomForestClassifier(random_state=rng)
 RF.fit(X_train, y_train)
 
-X_train, X_dsel, y_train, y_dsel = train_test_split(X, y, test_size=0.50,
+X_train, X_dsel, y_train, y_dsel = train_test_split(X_train, y_train,
+                                                    test_size=0.50,
                                                     random_state=rng)
 
 # Training a random forest to be used as the pool of classifiers.
@@ -109,7 +110,7 @@ ax.bar(np.arange(7),
         ola_score, mcb_score, meta_score],
        color=colors,
        tick_label=labels)
-ax.set_ylim(0.93, 1)
+ax.set_ylim(0.93, 0.97)
 ax.set_xlabel('Method', fontsize=13)
 ax.set_ylabel('Accuracy on the test set (%)', fontsize=13)
 ax.yaxis.set_major_formatter(pct_formatter)

examples/plot_xor_example.py
@@ -106,7 +106,7 @@ rng = np.random.RandomState(1234)
 X, y = make_xor(1000, random_state=rng)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,
                                                     random_state=rng)
-X_DSEL, X_test, y_DSEL, y_test = train_test_split(X_test, y_test,
+X_DSEL, X_test, y_DSEL, y_test = train_test_split(X_train, y_train,
                                                   test_size=0.5,
                                                   random_state=rng)
 
@@ -129,7 +129,7 @@ X_DSEL = np.vstack((X_DSEL, X_train))
 y_DSEL = np.hstack((y_DSEL, y_train))
 list_ds, names = initialize_ds(pool_classifiers, X_DSEL, y_DSEL, k=7)
 
-fig, sub = plt.subplots(4, 3, figsize=(15, 10))
+fig, sub = plt.subplots(4, 3, figsize=(13, 10))
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
 ax_data = sub.flatten()[0]
@@ -149,6 +149,7 @@ for ds, name, ax in zip(list_ds, names, sub.flatten()[2:]):
     ax.set_ylim((np.min(X_train[:, 1]) - 0.1, np.max(X_train[:, 1] + 0.1)))
     ax.set_title(name)
 plt.show()
+plt.tight_layout()
 
 ###############################################################################
 # Evaluation on the test set