de69069d334c3357f159668f403291dd25d9d344
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2019-01-23 19:28:30 -0500

    Faiss batch processing (#139)
    * change kneighbors function to return the distance matrix only when the flag return_distance=True
    
    * batch_processin on predict_proba
    
    * refactoring
    
    * Change class array dtype from int64 to int16
    
    * Checking if the estimator is fitted before usage

--- deslib/util/faiss_knn_wrapper.py
+++ deslib/util/faiss_knn_wrapper.py
@@ -5,6 +5,7 @@
 # License: BSD 3 clause
 
 import numpy as np
+from sklearn.utils.validation import check_is_fitted
 
 
 def is_available():
@@ -33,18 +34,14 @@ class FaissKNNClassifier:
 
     References
     ----------
-    Johnson, Jeff, Matthijs Douze, and Hervé Jégou. "Billion-scale similarity
-    search with gpus."
-    arXiv preprint arXiv:1702.08734 (2017).
+    Johnson Jeff, Matthijs Douze, and Hervé Jégou. "Billion-scale similarity
+    search with gpus." arXiv preprint arXiv:1702.08734 (2017).
     """
 
     def __init__(self, n_neighbors=5, n_jobs=None, algorithm=None):
         self.n_neighbors = n_neighbors
         self.n_jobs = n_jobs
         self.algorithm = algorithm
-        self.y = None
-        self.num_of_classes = None
-        self.index = None
 
         import faiss
         self.faiss = faiss
@@ -63,15 +60,15 @@ class FaissKNNClassifier:
         preds : array, shape (n_samples,)
                 Class labels for samples in X.
         """
-        _, idx = self.kneighbors(X, self.n_neighbors)
-        class_idx = self.y[idx]
+        idx = self.kneighbors(X, self.n_neighbors, return_distance=False)
+        class_idx = self.y_[idx]
         counts = np.apply_along_axis(
-            lambda x: np.bincount(x, minlength=self.num_of_classes), axis=1,
-            arr=class_idx.astype(np.int64))
+            lambda x: np.bincount(x, minlength=self.n_classes_), axis=1,
+            arr=class_idx.astype(np.int16))
         preds = np.argmax(counts, axis=1)
         return preds
 
-    def kneighbors(self, X, n_neighbors, return_distance=True):
+    def kneighbors(self, X, n_neighbors=None, return_distance=True):
         """Finds the K-neighbors of a point.
 
         Parameters
@@ -84,6 +81,9 @@ class FaissKNNClassifier:
             Number of neighbors to get (default is the value passed to the
             constructor).
 
+        return_distance : boolean, optional. Defaults to True.
+            If False, distances will not be returned
+
         Returns
         -------
         dists : list of shape = [n_samples, k]
@@ -94,9 +94,26 @@ class FaissKNNClassifier:
             Indices of the instances belonging to the region of competence of
             the given query sample.
         """
+        if n_neighbors is None:
+            n_neighbors = self.n_neighbors
+
+        elif n_neighbors <= 0:
+            raise ValueError("Expected n_neighbors > 0."
+                             " Got {}" .format(n_neighbors))
+        else:
+            if not np.issubdtype(type(n_neighbors), np.integer):
+                raise TypeError(
+                    "n_neighbors does not take {} value, "
+                    "enter integer value" .format(type(n_neighbors)))
+
+        check_is_fitted(self, 'index_')
+
         X = np.atleast_2d(X).astype(np.float32)
-        dist, idx = self.index.search(X, n_neighbors)
-        return dist, idx
+        dist, idx = self.index_.search(X, n_neighbors)
+        if return_distance:
+            return dist, idx
+        else:
+            return idx
 
     def predict_proba(self, X):
         """Estimates the posterior probabilities for sample in X.
@@ -111,16 +128,13 @@ class FaissKNNClassifier:
         preds_proba : array of shape = [n_samples, n_classes]
                           Probabilities estimates for each sample in X.
         """
-        _, idx = self.kneighbors(X, self.n_neighbors)
-        class_idx = self.y[idx]
+        idx = self.kneighbors(X, self.n_neighbors, return_distance=False)
+        class_idx = self.y_[idx]
         counts = np.apply_along_axis(
-            lambda x: np.bincount(x, minlength=self.num_of_classes), axis=1,
-            arr=class_idx.astype(np.int64))
-        preds = np.argmax(counts, axis=1)
+            lambda x: np.bincount(x, minlength=self.n_classes_), axis=1,
+            arr=class_idx.astype(np.int16))
 
-        preds_proba = np.zeros((X.shape[0], self.num_of_classes))
-        for i in range(preds.shape[0]):
-            preds_proba[i] = counts[i] / self.n_neighbors
+        preds_proba = counts / self.n_neighbors
 
         return preds_proba
 
@@ -137,8 +151,8 @@ class FaissKNNClassifier:
         """
         X = np.atleast_2d(X).astype(np.float32)
         X = np.ascontiguousarray(X)
-        self.index = self.faiss.IndexFlatL2(X.shape[1])
-        self.index.add(X)
-        self.y = y
-        self.num_of_classes = np.max(self.y) + 1
+        self.index_ = self.faiss.IndexFlatL2(X.shape[1])
+        self.index_.add(X)
+        self.y_ = y
+        self.n_classes_ = np.unique(y).size
         return self
