8f9eac18ef612dfbae5bb006b1baea4ef3918eef
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2019-11-03 03:59:41 -0500

    adding tests

--- deslib/tests/util/test_knne.py
+++ deslib/tests/util/test_knne.py
@@ -1,34 +1,65 @@
+import pytest
 import numpy as np
 from deslib.util import KNNE
 
 
-def test_equal_classes():
+def setup_test(n_neighbors):
     X = np.tile(np.arange(15).reshape(-1, 1), 3)
     y = np.array(5 * [0] + 5 * [1] + 5 * [2])
-
-    knne = KNNE(n_neighbors=6)
+    knne = KNNE(n_neighbors=n_neighbors)
     knne.fit(X, y)
+    return X, y, knne
+
+
+def test_equal_classes():
+    X, y, knne = setup_test(n_neighbors=6)
     dist, inds = knne.kneighbors()
     classes = y[inds]
     b = np.apply_along_axis(np.bincount, 1, classes)
     assert np.equal(b, 2).all()
 
 
+def test_uneven_k():
+    X, y, knne = setup_test(n_neighbors=7)
+    test = X[0, :].reshape(1, -1)
+    dist, inds = knne.kneighbors(test, 7)
+    assert np.allclose([0, 1, 5, 6, 10, 11, 2], inds)
+
+
 def test_predict():
-    X = np.tile(np.arange(10).reshape(-1, 1), 3)
-    y = np.array(5 * [0] + 5 * [1])
-    knne = KNNE(n_neighbors=6)
-    knne.fit(X, y)
+    X, y, knne = setup_test(n_neighbors=6)
     prediction = knne.predict(X)
     assert np.equal(prediction, y).all()
 
 
 def test_predict_proba():
-    X = np.tile(np.arange(10).reshape(-1, 1), 3)
-    y = np.array(5 * [0] + 5 * [1])
-
-    knne = KNNE(n_neighbors=6)
-    knne.fit(X, y)
+    X, y, knne = setup_test(n_neighbors=6)
     probas = knne.predict_proba(X)
     assert np.all(probas[0:5, 0] > probas[0:5, 1])
     assert np.all(probas[5:-1, 1] > probas[5:-1, 0])
+
+
+def test_labels_not_encoded():
+    X = np.tile(np.arange(10).reshape(-1, 1), 3)
+    y = np.array(5 * ['cat'] + 5 * ['dog'])
+    knne = KNNE(n_neighbors=6)
+    knne.fit(X, y)
+    dist, inds = knne.kneighbors()
+    classes = y[inds]
+    a = np.sum(classes=='dog')
+    b = np.sum(classes=='cat')
+    assert np.equal(b, a).all() and a==30
+
+
+def test_n_neighbors_none():
+    X = np.tile(np.arange(10).reshape(-1, 1), 3)
+    y = np.array(5 * ['cat'] + 5 * ['dog'])
+    knne = KNNE(n_neighbors=None)
+    with pytest.raises(ValueError):
+        knne.fit(X, y)
+
+
+def test_return_indices_only():
+    X, y, knne = setup_test(n_neighbors=6)
+    inds = knne.kneighbors(X, return_distance=False)
+    assert inds.shape == (15, 6)
