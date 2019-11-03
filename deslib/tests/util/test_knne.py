53bb26f801b4e9c43c6884b5b2f8318b0a5c84fe
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2019-11-02 20:33:44 -0400

    adding predict and predict_proba methods for knne

--- deslib/tests/util/test_knne.py
+++ deslib/tests/util/test_knne.py
@@ -12,3 +12,23 @@ def test_equal_classes():
     classes = y[inds]
     b = np.apply_along_axis(np.bincount, 1, classes)
     assert np.equal(b, 2).all()
+
+
+def test_predict():
+    X = np.tile(np.arange(10).reshape(-1, 1), 3)
+    y = np.array(5 * [0] + 5 * [1])
+    knne = KNNE(n_neighbors=6)
+    knne.fit(X, y)
+    prediction = knne.predict(X)
+    assert np.equal(prediction, y).all()
+
+
+def test_predict_proba():
+    X = np.tile(np.arange(10).reshape(-1, 1), 3)
+    y = np.array(5 * [0] + 5 * [1])
+
+    knne = KNNE(n_neighbors=6)
+    knne.fit(X, y)
+    probas = knne.predict_proba(X)
+    assert np.all(probas[0:5, 0] > probas[0:5, 1])
+    assert np.all(probas[5:-1, 1] > probas[5:-1, 0])
--- deslib/util/knne.py
+++ deslib/util/knne.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import check_X_y
+from sklearn.utils import check_array
+from deslib.util.prob_functions import softmax
 
 
 class KNNE(object):
@@ -27,20 +29,34 @@ class KNNE(object):
     Pattern Recognition 85 (2019): 149-160.
     """
 
-    def __init__(self, n_neighbors=7, **kwargs):
+    def __init__(self,
+                 n_neighbors=7,
+                 knn_classifier='sklearn',
+                 **kwargs):
 
         self.n_neighbors = n_neighbors
+        self.knn_classifier = knn_classifier
         self.kwargs = kwargs
 
     def fit(self, X, y):
+        """Fit the model according to the given training data.
 
+        Parameters
+        ----------
+        X : array of shape = [n_samples, n_features]
+            Data used to fit the model.
+
+        y : array of shape = [n_samples]
+            class labels of each example in X.
+        """
         X, y = check_X_y(X, y)
 
         self.knns_ = {}
         self.classes_indexes_ = {}
         self.X_ = X
         self.y_ = y
-        self.classes_ = set(y)
+        self.classes_ = np.unique(y)
+        self.n_classes = self.classes_.size
 
         if self.n_neighbors is not None:
             n_samples = {}
@@ -53,9 +69,10 @@ class KNNE(object):
                 n_samples[class_] = mdc + (1 if mod > 0 else 0)
                 mod = mod - 1
         else:
-            raise ValueError('either n_neighbors or n_neighbors_per_class')
+            raise ValueError('Either n_neighbors or n_neighbors_per_class'
+                             ' needs to be informed!')
 
-        for class_ in set(y):
+        for class_ in self.classes_:
             self.classes_indexes_[class_] = np.argwhere(
                 np.array(y) == class_).ravel()
             y_c = y[self.classes_indexes_[class_]]
@@ -87,7 +104,64 @@ class KNNE(object):
         a = np.tile(np.arange(b.shape[0]).reshape(-1, 1), (1, b.shape[1]))
 
         if return_distance:
-            return dists[a, b], inds[a, b]
+            return inds[a, b], dists[a, b],
         else:
             return inds[a, b]
 
+    def predict(self, X):
+        """Predict the class label for each sample in X.
+
+        Parameters
+        ----------
+
+        X : array of shape = [n_samples, n_features]
+            The input data.
+
+        Returns
+        -------
+        preds : array, shape (n_samples,)
+                Class labels for samples in X.
+        """
+        proba = self.predict_proba(X)
+        return self.classes_.take(np.argmax(proba, axis=1), axis=0)
+
+    def predict_proba(self, X):
+        """Predict probabilities.
+
+        As the
+
+        Parameters
+        ----------
+
+        X : array of shape = [n_samples, n_features]
+            The input data.
+
+        Returns
+        -------
+        preds : array, shape (n_samples,)
+                Class labels for samples in X.
+        """
+        X = check_array(X, accept_sparse='csr')
+
+        inds, dists = self.kneighbors(X, self.n_neighbors,
+                                      return_distance=True)
+        classes = self.y_[inds]
+        dists_array = np.empty((X.shape[0], self.n_classes))
+        # TODO: check if a more efficient implementation can be done
+        for c in self.classes_:
+            dists_array[:, c] = np.ma.MaskedArray(dists, classes != c).mean(
+                axis=1)
+        probas = softmax(1./dists_array)
+        return probas
+
+
+if __name__ == '__main__':
+    X = np.tile(np.arange(15).reshape(-1, 1), 3)
+    y = np.array(5 * [0] + 5 * [1] + 5 * [2])
+
+    knne = KNNE(n_neighbors=8)
+    knne.fit(X, y)
+    dist, inds = knne.kneighbors()
+    classes = y[inds]
+    b = np.apply_along_axis(np.bincount, 1, classes)
+    assert np.equal(b, 2).all()
