97340794f69a1a66b1759109b0885b043daff8bf
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2020-05-21 02:31:25 -0400

    removing main

--- deslib/util/fire.py
+++ deslib/util/fire.py
@@ -1,4 +1,18 @@
-"""Implementation of the FIRE-DES algorithm."""
+"""Implementation of the Dynamic Frienemy Pruning (DFP) algorithm for online
+pruning of base classifiers.
+
+
+References
+----------
+Oliveira, D.V.R., Cavalcanti, G.D.C. and Sabourin, R., Online Pruning
+of Base Classifiers for Dynamic Ensemble Selection,
+Pattern Recognition, vol. 72, December 2017, pp 44-58.
+
+Cruz, Rafael MO, Dayvid VR Oliveira, George DC Cavalcanti, and Robert Sabourin.
+"FIRE-DES++: Enhanced online pruning of base classifiers for dynamic ensemble
+selection." Pattern Recognition 85 (2019): 149-160.
+
+"""
 
 # coding=utf-8
 
@@ -36,11 +50,6 @@ def frienemy_pruning(X_query, X_dsel, y_dsel, ensemble, k):
     DFP_mask : array-like of shape = [n_samples, n_classifiers]
                Mask containing 1 for the selected base classifier and 0
                otherwise.
-    References
-    ----------
-    Oliveira, D.V.R., Cavalcanti, G.D.C. and Sabourin, R., Online Pruning
-    of Base Classifiers for Dynamic Ensemble Selection,
-    Pattern Recognition, vol. 72, December 2017, pp 44-58.
 
     """
     predictions = np.zeros((X_dsel.shape[0], len(ensemble)),
@@ -80,11 +89,6 @@ def frienemy_pruning_preprocessed(neighbors, y_val, hit_miss):
     DFP_mask : array-like of shape = [n_samples, n_classifiers]
                Mask containing 1 for the selected base classifier and 0
                otherwise.
-    References
-    ----------
-    Oliveira, D.V.R., Cavalcanti, G.D.C. and Sabourin, R., Online Pruning
-    of Base Classifiers for Dynamic Ensemble Selection,
-    Pattern Recognition, vol. 72, December 2017, pp 44-58.
     """
     n_samples = neighbors.shape[0]
     n_classifiers = hit_miss.shape[1]
@@ -102,27 +106,11 @@ def frienemy_pruning_preprocessed(neighbors, y_val, hit_miss):
                 [mask] = np.where(hit_miss[curr_neighbors, clf_index])
                 if len(set(neighbors_y[mask])) > 1:
                     dfp_mask[sample_idx, clf_index] = 1.0
-
-            # Check if all classifiers were pruned
-            if not np.count_nonzero(dfp_mask[sample_idx, :]):
-                # Do not apply the pruning mechanism.
-                dfp_mask[sample_idx, :] = 1.0
         else:
             # The sample is located in a safe region. All base classifiers
             # can predict the label
             dfp_mask[sample_idx, :] = 1.0
 
+    # Check if all classifiers were pruned
+    dfp_mask[np.all(dfp_mask == 0, axis=1)] = 1.0
     return dfp_mask
-
-
-# USed for profiling
-if __name__ == '__main__':
-    from sklearn.datasets import make_classification
-    from sklearn.linear_model import Perceptron
-    from sklearn.ensemble import BaggingClassifier
-
-    X, y = make_classification(n_samples=10000)
-    pool = BaggingClassifier(Perceptron(max_iter=10), n_estimators=100,
-                             random_state=42).fit(X, y)
-    mask = frienemy_pruning(X, X, y, pool, 7)
-    print("aaa")
