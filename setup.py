3018743b4205fbaaf5d606987633ed863d7ed392
Rafael Menelau Oliveira e Cruz <rafaelmenelau@gmail.com>
2021-02-07 19:56:12 -0500

    version 0.3.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(os.path.join(setup_path, 'README.rst'), encoding='utf-8-sig') a
     README = f.read()
 
 setup(name='DESlib',
-      version='0.4.dev',
+      version='0.3.5',
       url='https://github.com/Menelau/DESlib',
       maintainer='Rafael M. O. Cruz, L. G. Hafemann',
       maintainer_email='rafaelmenelau@gmail.com',
