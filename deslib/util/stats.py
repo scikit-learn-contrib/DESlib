3a764e4453ccaf0284d9194ea259c2ef83b557f7
Pierre-Marc Thibault <pierre-marc.thibault.1@etsmtl.qc.ca>
2021-03-11 21:39:36 -0500

    Segmentation du log.

--- deslib/util/stats.py
+++ deslib/util/stats.py
@@ -10,44 +10,66 @@ class Stats():
         self.predicted_labels = []
         self.agree_labels = []
         self.competences = []
+        self.log_fname = "log.txt"
 
     def log_stats(self):
-        n_queries = len(self.true_labels)
-        n_classes = len(np.unique(self.predicted_labels))
-        n_bases = len(self.bases_labels[0])
-        n_agree = len(self.agree_ind)
-        n_disagree = len(self.disagree_ind)
+        self.n_queries = len(self.true_labels)
+        self.n_bases = len(self.bases_labels[0])
+        self.n_disagree = len(self.disagree_ind)
         
-        n_right_clf_by_query, n_right_clf_ind = \
-            self._get_n_right_clf_stats(n_bases)
+        with open(self.log_fname,'w') as f:
+            for line in self._get_all_lines():
+                f.write(str(line))
+                f.write("\n")
 
-        predicted_dis = self._get_distribution()
-        agree_dis = self._get_distribution(ind=self.agree_ind)
-        n_right_clf_dis = self._get_distribution(n_right_clf_by_query)
-        
-        agree_score = self._get_score(self.agree_ind)
-        disagree_score = self._get_score(self.disagree_ind)
-        
-        competences_mean, competences_mean_by_clf, n_even_max_competence = \
-            self._get_competences_stats()
-
+    def _get_all_lines(self):
         lines = []
-        lines.extend([
+        lines.extend(self._get_general_lines())
+        lines.extend(self._get_agree_lines())
+        lines.extend(self._get_n_right_clf_lines())
+        lines.extend(self._get_disagree_lines())
+        lines.extend(self._get_competences_lines())
+        return lines
+
+    def _get_general_lines(self):
+        lines = [
             "Queries:",
-            n_queries,
-            "Nb of right classifiers from 0 to "+str(n_bases)+":",
-            n_right_clf_dis,
+            self.n_queries,
+        ]
+
+        return lines
+
+    def _get_agree_lines(self):
+        n_agree = len(self.agree_ind)
+        agree_dis = self._get_distribution(ind=self.agree_ind)
+        agree_score = self._get_score(self.agree_ind)
+        predicted_dis = self._get_distribution()
+        
+        lines = [
             "--- Agreements",
             "Instances, ratio on queries:",
             n_agree,
-            round(n_agree / n_queries, 3),
+            round(n_agree / self.n_queries, 3),
             "Classes distribution, ratio on predictions:",
             agree_dis,
             np.round(agree_dis / predicted_dis, 3),
             "Score, ratio on agreements:",
             agree_score,
             round(agree_score / n_agree, 3),
-        ])
+        ]
+
+        return lines
+
+    def _get_n_right_clf_lines(self):
+        n_right_clf_by_query, n_right_clf_ind = \
+            self._get_n_right_clf_stats()
+        n_right_clf_dis = self._get_distribution(n_right_clf_by_query)
+
+        lines = [
+            "--- Right classifiers:",
+            "Distribution:",
+            n_right_clf_dis,
+        ]
 
         for i,n_right_clf in enumerate(n_right_clf_dis):
             score = self._get_score(n_right_clf_ind[i])
@@ -55,20 +77,34 @@ class Stats():
                 "--- "+str(i)+" right classifiers",
                 "Instances, ratio on queries:",
                 n_right_clf_dis[i],
-                round(n_right_clf / n_queries, 3),
+                round(n_right_clf / self.n_queries, 3),
                 "Score, ratio on "+str(i)+" right clf:",
                 score,
                 round(score / n_right_clf_dis[i], 3),
             ])
 
-        lines.extend([
+        return lines
+
+    def _get_disagree_lines(self):
+        disagree_score = self._get_score(self.disagree_ind)
+
+        lines = [
             "--- Disagreements",
             "Instances, ratio on queries:",
-            n_disagree,
-            round(n_disagree / n_queries, 3),
+            self.n_disagree,
+            round(self.n_disagree / self.n_queries, 3),
             "Score, ratio on disagreements:",
             disagree_score,
-            round(disagree_score / n_disagree, 3),
+            round(disagree_score / self.n_disagree, 3),
+        ]
+
+        return lines
+
+    def _get_competences_lines(self):
+        competences_mean, competences_mean_by_clf, n_even_max_competence = \
+            self._get_competences_stats()
+
+        lines = [
             "--- Competences",
             "Mean:",
             round(competences_mean, 3),
@@ -76,13 +112,10 @@ class Stats():
             np.round(competences_mean_by_clf, 3),
             "Even max competences times, ratio on disagreements:",
             n_even_max_competence,
-            round(n_even_max_competence / n_disagree, 3),
-        ])
-        
-        with open("log.txt",'w') as f:
-            for line in lines:
-                f.write(str(line))
-                f.write("\n")
+            round(n_even_max_competence / self.n_disagree, 3),
+        ]
+
+        return lines
 
     def _get_distribution(self, labels=None, ind=None):
         labels = self.predicted_labels if labels is None else labels
@@ -90,9 +123,9 @@ class Stats():
         _, counts = np.unique(labels, return_counts=True)
         return counts
 
-    def _get_n_right_clf_stats(self, n_bases):
+    def _get_n_right_clf_stats(self):
         n_right_clf_by_query = []
-        n_right_clf_ind = [[] for i in range(n_bases + 1)]
+        n_right_clf_ind = [[] for i in range(self.n_bases + 1)]
 
         for i,label in enumerate(self.true_labels):
             row = self.bases_labels[i]
